4f735fca9fd51b2646c298cd6c8c352809364f63
Kseniya Kuzina <kolosova_ka@mail.ru>
2025-08-17 12:47:06 +0300

    Update processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,29 +1,30 @@
 from typing import Dict, List
 
 
-def filter_by_state(list_of_dicts: List[Dict], state: str) -> List:
-    new_list = []
+def filter_by_state(list_of_dicts: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+    """Функция, которая сортирует данные по статусу 'state'"""
+    sorted_list = []
     for element in list_of_dicts:
         for key, value in element.items():
             if element[key] == state:
-                new_list.append(element)
-    return new_list
+                sorted_list.append(element)
+    return sorted_list
 
 
-my_list = [
+user_list = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
-print(filter_by_state(my_list, "EXECUTED"))
+print(filter_by_state(user_list))
 
 
-def sort_by_date(list_of_dicts: List[Dict], reverse: bool) -> List:
+def sort_by_date(list_of_dicts: List[Dict], is_reverse: bool = True) -> List:
     """Функция, которая сортирует список словарей по дате в порядке убывания"""
     sorted_list_of_dicts = sorted(list_of_dicts, key=lambda element: element["date"], reverse=True)
     return sorted_list_of_dicts
 
 
-print(sort_by_date(my_list, reverse=True))
+print(sort_by_date(user_list, is_reverse=True))
