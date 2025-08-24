5eb4916f6d515cc710403a3019da691449ceedf3
Kseniya Kuzina <kolosova_ka@mail.ru>
2025-08-24 20:26:21 +0300

    дополнение README тестированием, изменение test_masks, test_widget

--- src/widget.py
+++ src/widget.py
@@ -33,5 +33,6 @@ def get_date(user_input: str) -> str:
 # Функция проверки работы кода
 if __name__ == "__main__":
     print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Maestro 7000792289606361"))
     print(mask_account_card("Счет 73654108430135874305"))
     print(get_date("2024-03-11T02:26:18.671407"))
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,3 +1,33 @@
 import pytest
 
-from src.widget import get_date, get_mask_account
+from src.widget import mask_account_card, get_date
+
+
+@pytest.fixture
+def number_card_for_widget():
+    return "Visa Platinum 7000792289606361"
+
+
+@pytest.fixture
+def number_account_for_widget():
+    return "Счёт 73654108430135874305"
+
+
+@pytest.fixture
+def date():
+    return "2024-03-11T02:26:18.671407"
+
+
+@pytest.mark.parametrize("expected", ["Visa Platinum 7000 79** **** 6361"])
+def test_mask_account_card(number_card_for_widget, expected):
+    assert mask_account_card(number_card_for_widget) == expected
+
+
+@pytest.mark.parametrize("expected", ["Счёт **4305"])
+def test_mask_account_card(number_account_for_widget, expected):
+    assert mask_account_card(number_account_for_widget) == expected
+
+
+@pytest.mark.parametrize("expected", ["11.03.2024"])
+def test_get_date(date, expected):
+    assert get_date(date) == expected
