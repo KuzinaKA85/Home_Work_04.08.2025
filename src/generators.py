3ccf771155fb84a64ba26523518bda85d3a69a14
Kseniya Kuzina <kolosova_ka@mail.ru>
2025-08-30 20:53:54 +0300

    Обновление функции filter_by_currency, инициализация функции transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -15,7 +15,7 @@ transactions = [
         "state": "EXECUTED",
         "date": "2018-06-30T02:08:58.425572",
         "operationAmount": {"amount": "9824.07", "currency": {"name": "EURO", "code": "EURO"}},
-        "description": "Перевод организации",
+        "description": "Перевод со счета на счет",
         "from": "Счет 75106830613657916952",
         "to": "Счет 11776614605963066702",
     },
@@ -27,6 +27,24 @@ transactions = [
         "description": "Перевод со счета на счет",
         "from": "Счет 19708645243227258542",
         "to": "Счет 75651667383060284188",
+    },
+    {
+        "id": 939711111,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "EURO", "code": "EURO"}},
+        "description": "Перевод с карты на карту",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    },
+    {
+        "id": 939717555,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
     },
 ]
 
@@ -41,6 +59,19 @@ def filter_by_currency(transactions_: List[Dict], name: str) -> Iterable:
 
 
 usd_transactions = filter_by_currency(transactions, "USD")
-
 for item in usd_transactions:
+    print(item)
+
+
+def transaction_descriptions(my_list: List[Dict]) -> Iterable:
+    """Функция принимает список словарей с транзакциями на вход, возвращает описание
+    каждой операции по очереди"""
+    for element in my_list:
+        yield element["description"]
+
+
+descriptions = transaction_descriptions(transactions)
+
+
+for item in descriptions:
     print(item)
