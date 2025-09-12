7bc77218fb021c7463366a7215b4d673ac94fa0e
Kseniya Kuzina <kolosova_ka@mail.ru>
2025-09-12 18:26:12 +0300

    init external_api.py (vers_2)

--- src/external_api.py
+++ src/external_api.py
@@ -1,11 +1,8 @@
-import json
 import os
-from typing import Dict, List
+from typing import Any, Dict
 
 import requests
 from dotenv import load_dotenv
-
-from src.utils import filepath, transaction_data
 
 load_dotenv()
 
@@ -15,34 +12,34 @@ if API_KEY is None:
     print("API_KEY не найден")
 
 
-def converted_transactions(transactions: List[Dict]) -> None | float | str:
-    """Фуекция принимает на вход транзакции и возвращает сумму в рублях. Если транзакция была в USD,
+def convert(transaction: Dict) -> Any:
+    """Функция принимает на вход транзакции и возвращает сумму в рублях. Если транзакция была в USD,
     происходит обращение к внешнему API для получения текущего курса валют и конвертации"""
-    try:
-        for dictionary in transactions:
-            if dictionary:
-                if dictionary["operationAmount"]["currency"]["code"] == "RUB":
-                    amount = float(dictionary["operationAmount"]["amount"])
-                    return amount
-                    currency_to = "RUB"
-                    currency_from = "USD"
-                    URL = f"https://api.apilayer.com/exchangerates_data/convert?to={currency_to}&from={currency_from}&amount={amount}"
-                    payload = {}
-                    headers = {"apikey": API_KEY}
-                    if currency_from:
-                        response = requests.request("GET", URL, headers=headers, data=payload)
-                        status_code = response.status_code
-                        result = response.text
-                        print(result)
-                        if status_code == 200:
-                            python_response = json.loads(result)
-                            print(python_response)
-                        else:
-                            print(f"Ошибка API: {response.status_code}")
-    except json.JSONDecodeError as e:
-        print(f"Ошибка декодирования JSON: {e}")
-    except KeyError:
-        return "Ключ не найден."
+
+    currency = transaction["operationAmount"]["currency"]["code"]
+    amount = transaction["operationAmount"]["amount"]
+    if currency == "RUB":
+        return amount
+
+    url = "https://api.apilayer.com/exchangerates_data/convert"
+    headers = {"apikey": API_KEY}
+    params = {"from": currency, "to": "RUB", "amount": amount}
+
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()["result"]
 
 
-print(converted_transactions(transaction_data(filepath)))
+if __name__ == "__main__":
+    print(
+        convert(
+            {
+                "date": "2019-08-16T04:23:41.621065",
+                "description": "Перевод с карты на счет",
+                "from": "MasterCard 8826230888662405",
+                "id": 86608620,
+                "operationAmount": {"amount": "6004.00", "currency": {"code": "USD", "name": "руб."}},
+                "state": "EXECUTED",
+                "to": "Счет 96119739109420349721",
+            }
+        )
+    )
