2ae9f306bdcc0d49eee5e9a207edc71f63bec7e4
Kseniya Kuzina <kolosova_ka@mail.ru>
2025-09-13 21:42:07 +0300

    init test_external_api.py

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,6 +1,8 @@
-import pytest
-from unittest.mock import patch, MagicMock
 from typing import Dict
+from unittest.mock import MagicMock, patch
+
+import pytest
+
 from src.external_api import convert
 
 
@@ -8,19 +10,19 @@ from src.external_api import convert
 def transaction() -> Dict:
     """Фикстура с тестовыми транзакциями."""
     return {
-            "date": "2019-08-16T04:23:41.621065",
-            "description": "Перевод с карты на счет",
-            "from": "MasterCard 8826230888662405",
-            "id": 86608620,
-            "operationAmount": {"amount": 6004.0, "currency": {"code": "USD", "name": "руб."}},
-            "state": "EXECUTED",
-            "to": "Счет 96119739109420349721",
+        "date": "2019-08-16T04:23:41.621065",
+        "description": "Перевод с карты на счет",
+        "from": "MasterCard 8826230888662405",
+        "id": 86608620,
+        "operationAmount": {"amount": 6004.0, "currency": {"code": "USD", "name": "руб."}},
+        "state": "EXECUTED",
+        "to": "Счет 96119739109420349721",
     }
 
 
 def test_convert(transaction):
     """Проверка корректного результат"""
     mock_response = MagicMock()
-    mock_response.json.return_value = {"result": 510340.0}
-    with patch('requests.get', return_value = mock_response):
-        assert convert(transaction) == 510340.0
+    mock_response.json.return_value = {"result": 502844.78652}
+    with patch("requests.get", return_value=mock_response):
+        assert convert(transaction) == 502844.78652
