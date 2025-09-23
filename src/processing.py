087623ea628b713dd310295dd1bfe7a97ddc56bd
Kseniya Kuzina <kolosova_ka@mail.ru>
2025-09-23 20:43:00 +0300

    init process_bank_search, process_bank_operations

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,5 @@
+import re
+from collections import Counter
 from typing import Any, Dict, List
 
 
@@ -32,6 +34,40 @@ def sort_by_date(list_of_dicts: List[Dict], reverse: bool) -> Any:
         else:
             sorted_list_of_dicts = sorted(list_of_dicts, key=lambda element: element["date"], reverse=True)
             return sorted_list_of_dicts
+
+
+def process_bank_search(data: list[dict], search: str) -> list[dict]:
+    """
+    Фильтрует список банковских операций, оставляя только те,
+    в описании которых содержится указанная строка поиска.
+    """
+    pattern = re.compile(re.escape(search), re.IGNORECASE)
+
+    result = []
+    for operation in data:
+
+        desc = str(operation.get("description", ""))
+        if pattern.search(desc):
+            result.append(operation)
+    return result
+
+
+def process_bank_operations(data: list[dict], categories: list = "") -> dict:
+    """
+    принимать список словарей с данными о банковских операциях и
+    список категорий операций, а возвращать словарь, в котором ключи
+    — это названия категорий, а значения — это количество операций
+    в каждой категории
+    """
+    categories_from_data = []
+
+    for operation in data:
+        desc = str(operation.get("description", ""))
+        if desc in categories:
+            categories_from_data.append(desc)
+
+    result = Counter(categories_from_data)
+    return result
 
 
 # Функция проверки работы кода
